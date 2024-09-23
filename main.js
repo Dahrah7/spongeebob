c0aa534fb19673730ed2c4010d006587f82c0efb
Dahrah7 <mobolajioluwadarasimi45@gmail.com>
2024-09-23 13:51:31 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function show(){
+    document.getElementById("demo").innerHTML ="  <br>Nickelodeon <br><br> youtube <br> <br>animation <br><br> netflix <br><br>showmax<br>"
+}
+function hide(){
+    document.getElementById("demo").innerHTML ="";
+}
